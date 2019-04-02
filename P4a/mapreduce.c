3864b216084119aaa0e4a263930bf89b3ccfebcb
Devang Maskara <maskara@emperor-05.cs.wisc.edu>
2019-04-01 20:53:14 -0500

    Sorting done and reducer threads created

--- P4a/mapreduce.c
+++ P4a/mapreduce.c
@@ -1,7 +1,9 @@
 // Code for P4a
-#include <assert.h>
+// Free allocated memory
+// Write get_next function
+// Maybe put everything in a struct (global vars)
+// Check for the issue of words with no length
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
@@ -12,12 +14,12 @@ struct pairs {
 	char *value;
 };
 
-int curCount = 0;
 struct pairs** partitions;
 int* pairCountInPartition;
 int* pairAllocatedInPartition;
 pthread_mutex_t lock;
 Partitioner p;
+Reducer r;
 Mapper m;
 int numberPartitions;
 
@@ -28,9 +30,26 @@ void* mapperHelper(void *arg) {
 	return arg;
 }
 
+// Helper function to be called by pthread_create which calls the get_next function for each reducer
+void* reducerHelper(void *arg) {
+	int* partitionNumber = (int *)arg;
+	printf("%d\n", *partitionNumber);
+	return arg;
+}
+
+// Sort the buckets by key and then by value in ascending order
+int compare(const void* p1, const void* p2) {
+	struct pairs *pair1 = (struct pairs*) p1;
+	struct pairs *pair2 = (struct pairs*) p2;
+	if (strcmp(pair1->key, pair2->key) == 0) {
+		return strcmp(pair1->value, pair2->value);
+	}
+	return strcmp(pair1->key, pair2->key);
+}
+
 void MR_Emit(char *key, char *value) {
-	// Check for words with no length
 	pthread_mutex_lock(&lock); 
+	// Checking to ensure words have a length
 	if(strlen(key)!= 0) {
 		// Getting the partition number
 		unsigned long hashPartitionNumber = p(key, numberPartitions);
@@ -53,19 +72,23 @@ void MR_Run(int argc, char *argv[], Mapper map, int num_mappers, Reducer reduce,
 
 	//Initialising all the required variables
 	pthread_t mapperThreads[num_mappers];
+	pthread_t reducerThreads[num_reducers];
 	pthread_mutex_init(&lock, NULL);
 	p = partition;
 	m = map;
+	r = reduce;
 	numberPartitions = num_reducers;
 	partitions = malloc(num_reducers * sizeof(struct pairs*));
 	pairCountInPartition = malloc(num_reducers * sizeof(int));
 	pairAllocatedInPartition = malloc(num_reducers * sizeof(int));
+	int arrayPosition[num_reducers];
 
 	// Initialising the arrays needed to store the key value pairs in the partitions
 	for(int i = 0; i < num_reducers; i++) {
-		partitions[i] = (struct pairs*)malloc(1024 * sizeof(struct pairs));
+		partitions[i] = malloc(1024 * sizeof(struct pairs));
 		pairCountInPartition[i] = 0;
 		pairAllocatedInPartition[i] = 1024;
+		arrayPosition[i] = i;
 	}
 
 	// Creating the threads for the number of mappers
@@ -80,6 +103,11 @@ void MR_Run(int argc, char *argv[], Mapper map, int num_mappers, Reducer reduce,
 		pthread_join(mapperThreads[i], NULL); 
 	}
 
+
+	for(int i = 0; i < num_reducers; i++) {
+		qsort(partitions[i], pairCountInPartition[i], sizeof(struct pairs), compare);
+	}
+
 	// Printing to debug
 	for(int i = 0; i < num_reducers; i++) {
 		printf("Reducer number: %d\n", i);
@@ -88,6 +116,19 @@ void MR_Run(int argc, char *argv[], Mapper map, int num_mappers, Reducer reduce,
 			printf("%s\n", (partitions[i][j].value));
 		}
 	}
+
+	// Creating the threads for the number of reducers
+	for (int i = 0; i < num_reducers; i++){
+	    if(pthread_create(&reducerThreads[i], NULL, reducerHelper, &arrayPosition[i])) {
+	    	printf("Error\n");
+	    }
+	}
+
+	//Waiting for the threads to finish
+	for(int i = 0; i < num_reducers; i++) {
+		pthread_join(reducerThreads[i], NULL); 
+	}
+
 	pthread_mutex_destroy(&lock);
 }
 
